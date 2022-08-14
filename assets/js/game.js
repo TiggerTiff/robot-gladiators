b7d6ea2a43966a28978ec386a762c617f0304598
TiggerTiff <tiffanybell031@gmail.com>
2022-08-14 11:57:29 +1000

    update fight function with while loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,68 +14,61 @@ for (var i = 0; i < enemyNames.length; i++) {
     // call fight function with enemy-robot
 }
 var fight = function(enemyName) {
-   // repeat and execute as long as the enemy-robot is alive
-   while(enemyHealth > 0) {
-    console.log(enemyHealth);
-    // place fight function code block here ...
-   }
+   while(enemyHealth > 0 && enemyHealth > 0) {
+    // ask player if they'd like to fight or run
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+
+ // if player choses to "Skip" confirm and then stop the loop
+ if (promptFight === "skip" || promptFight === "SKIP") {
+     // confirm player wants to skip
+     var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+ //if yes (true), leave fight
+ if (confirmSkip) {
+    window.alert(playerName + " has decided to skip this fight. Goodbye!");
+        //subtract money from playerMoney for skipping
+        playerMoney = playerMoney - 10;
+        console.log("playerMoney", playerMoney); 
+    break;
+ }
+
 }
 
-// create function
-
-   
-var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
-console.log (promptFight);
-
-// if player choses to fight, then fight
-if (promptFight === "fight" || promptFight === "FIGHT") {
-// remove enemy's health by subtracting the amount set in the playerAttack variable 
+// remove enemy's health by subtracting the amount set in the playerAttack variable
 enemyHealth = enemyHealth - playerAttack;
 console.log(
-    playerName + " attacked " + enemyNames + ". " + enemyNames + " now has " + enemyHealth + " health remaining."
-  );
+    playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
+);
 
-  // check enemy's health
-  if (enemyHealth <= 0) {
-    window.alert(enemyName + " has died!");
+// check enemy's health
+if (enemyHealth <= 0) {
+    window.alert(enemyName + 'has died!');
+
+    //award player money for winning
+    playerMoney = playerMoney + 20;
+    // leave while() loop since enemy is dead
+    break;
   } else {
-    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    window.alert(enemyName + ' still has ' + enemyHealth + ' health left.');
   }
+
 
   // remove player's health by subtracting the amount set in the enemyAttack variable
   playerHealth = playerHealth - enemyAttack;
   console.log(
-    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining. '
   );
 
 // check player's health
 if (playerHealth <= 0) {
-    window.alert(playerName + " has died!");
-  } else {
-    window.alert(playerName + " still has " + playerHealth + " health left.");
-  }
-  // if player choses to skip
-} else if (promptFight === "skip" || promptFight === "SKIP") {
-     // confirm player wants to skip
-     var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-
-     //if yes (true), leave fight
-     if (confirmSkip) {
-         window.alert(playerName + " has decided to skip this fight. Goodbye!");
-             //subtract money from playerMoney for skipping
-             playerMoney = playerMoney - 2;
-     }
-     // if no (false), ask question again by running fight() again
-     else {
-        for(var i = 0; i < enemyNames.length; i++) {
-           var pickedEnemyName = enemyNames[i];
-           enemyHealth = 50;
-            fight(pickedEnemyName);
-        } 
-     } 
-    
+    window.alert(playerName + ' has died!');
+    // leave while () loop if player is dead
+    break;
+}else{
+    window.alert(playerName + ' still has ' + playerHealth + ' health left.');
 }
 
+} // end of while
 
+}; // end of fight function
 
-   
