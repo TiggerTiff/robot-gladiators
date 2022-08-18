f422e11c2e90465f545dae382de39d05a7cd44ad
TiggerTiff <tiffanybell031@gmail.com>
2022-08-19 08:10:36 +1000

    change shop input value to numeric

--- assets/js/game.js
+++ assets/js/game.js
@@ -95,34 +95,29 @@ var randomNumber = function() {
     return value;
 };
 
+
 var shop = function () {
    // ask player what they'd like to do 
    var shopOptionPrompt = window.prompt(
     "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: ' REFILL' , 'UPGRADE' , or 'LEAVE' to make a choice." 
     );
 
-    // use switch to carry out actiom
+    // use switch to carry out action
+    var shopOptionPrompt = window.prompt(
+        "Would you like to REFILL your health, UPGRADE youe attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
+    )
     switch (shopOptionPrompt) {
-        case "REFILL": //new case
-        case "refill":
-
+        case 1:
             playerInfo.refillHealth();
         break;
-    case "UPGRADE": //new case
-    case "upgrade":
-         playerInfo.upgradeAttack();
-        
+        case 2:
+         playerInfo.upgradeAttack(); 
         break; 
-    case "LEAVE": // new case
-    case "leave":
+        case 3:
         window.alert("Leaving the store.");
-
-        //do ntohing, so function will end 
         break;
-    default:
+        default:
         window.alert("You did not pick a valid option. Try again.");
-
-        // call shop() again to force player to pick a valid option
         shop();
         break;
 
